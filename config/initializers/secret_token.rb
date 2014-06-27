7a5e7ecfaedc31193349eaea98f62d15d4f53e04
danielsampete <danielsam.pete@chronus.com>
2014-06-27 14:37:25 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sms::Application.config.secret_token = '92584390a624179f6a11aa4b27b55243c5f7272c910fb2228b7fd38c68c87b867e52869ae2c8b8e403cb2d22a6ab3e99c57725250edf30225e2a842856c600b9'
